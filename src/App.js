408b5e34d6bb71b20f09917774dd4d697f373077
Roberto Guevara <robertoeguevara@gmail.com>
2022-08-10 13:33:56 -0700

    Data fetching with a Fetch Hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './App.css';
-import {useTrees} from "./";
 
 function App() {
   
- const trees = useTrees();
+ 
 
   return (
     <div>
-      <h1>Trees I've Heard Of</h1>
-      <ul>
-        {trees.map((tree) => (
-          <li key={tree.id}>{tree.type}</li>
-        ))}
-      </ul>
+     
     </div>
   );
 }
