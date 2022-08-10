0331074853041a53fbba4f7d56ce2be5bb7b0cc8
Roberto Guevara <robertoeguevara@gmail.com>
2022-08-09 23:42:21 -0700

    useState for form inputs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState, useEffect } from 'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { FaStar } from 'react-icons/fa';
+
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users')
+      .then((response) => response.json())
+      .then(setData);
+  });
+
+  if (data){
+    return (
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.login}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <p>No users</p>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +35,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
