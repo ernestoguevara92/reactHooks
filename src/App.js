024745536228186807ba108aaf51c26d51a27978
Roberto Guevara <robertoeguevara@gmail.com>
2022-08-10 12:53:50 -0700

    creating a custom hook with context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import './App.css';
-import {TreesContext} from "./";
+import {useTrees} from "./";
 
 function App() {
   
-  const result = useContext(TreesContext);
-  console.log(result);
+ const trees = useTrees();
 
   return (
     <div>
       <h1>Trees I've Heard Of</h1>
       <ul>
-        {result.map((tree) => (
+        {trees.map((tree) => (
           <li key={tree.id}>{tree.type}</li>
         ))}
       </ul>
